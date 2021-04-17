456cd69e82dcf459bc6801a43a666bd7c556be5a
lrq619 <lrq619@sjtu.edu.cn>
2021-04-17 23:43:57 +0800

    [feat]change ip in PostBuildingInfo to localhost

--- Hubs/MainHub.cs
+++ Hubs/MainHub.cs
@@ -79,7 +79,7 @@ namespace SignalRServer.Hubs
        
         public string PostBuildingInfo(string groupName,string postDataStr)
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://124.71.176.240:8000/api/v1/data/buildinfo");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://127.0.0.1:8000/api/v1/data/buildinfo");
             request.Method = "POST";
             request.ContentType = "application/x-www-form-urlencoded";
             request.ContentLength = Encoding.UTF8.GetByteCount(postDataStr);
